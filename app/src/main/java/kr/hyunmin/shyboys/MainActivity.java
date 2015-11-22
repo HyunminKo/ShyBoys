c92779fed880eee682894dd2d37794b24f70b34a
Kohyunmin <hyunmin4578@gmail.com>
2015-11-22 23:53:53 +0900

    Main Mail icon delete

--- app/src/main/java/kr/hyunmin/shyboys/MainActivity.java
+++ app/src/main/java/kr/hyunmin/shyboys/MainActivity.java
@@ -1,11 +1,8 @@
 package kr.hyunmin.shyboys;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,15 +15,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        setTitle("샤이보이");
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
 
     @Override
