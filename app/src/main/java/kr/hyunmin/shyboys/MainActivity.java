b1a1b01cccc35726f39f8d59aa1344cb12df305d
Takoyakiki <hjj1210@naver.com>
2015-11-23 20:44:24 +0900

    기본

--- app/src/main/java/kr/hyunmin/shyboys/MainActivity.java
+++ app/src/main/java/kr/hyunmin/shyboys/MainActivity.java
@@ -1,16 +1,28 @@
 package kr.hyunmin.shyboys;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button join_Button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        join_Button=(Button)findViewById(R.id.join_Button);
+        join_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this,QuestionActivity.class);
+                startActivity(intent1);
+            }
+        });
 
     }
 
