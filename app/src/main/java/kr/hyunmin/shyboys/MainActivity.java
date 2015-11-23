7ab031e9cd09173024245827b0b2c437f1f39539
Kohyunmin <hyunmin4578@gmail.com>
2015-11-23 21:19:30 +0900

    로그인 팝업, 회원가입 팝업 완료 ( 로그인 하는 함수, 회원가입 하는 함수 짜야함)

--- app/src/main/java/kr/hyunmin/shyboys/MainActivity.java
+++ app/src/main/java/kr/hyunmin/shyboys/MainActivity.java
@@ -1,18 +1,15 @@
 package kr.hyunmin.shyboys;
 
 
-import android.content.Intent;
-
 import android.content.Context;
 import android.content.DialogInterface;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.view.ViewGroup;
 import android.widget.Button;
 
@@ -25,9 +22,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         start_button = (Button)findViewById(R.id.start_Button);
-
-
         join_Button=(Button)findViewById(R.id.join_Button);
+
         join_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,39 +31,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
-
-
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Context mContext = getApplicationContext();
-                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-
-                //R.layout.init는 xml 파일명이고  R.id.popup은 보여줄 레이아웃 아이디
-                View layout = inflater.inflate(R.layout.loginpopup,(ViewGroup) findViewById(R.id.popup));
-                final android.app.AlertDialog.Builder aDialog = new android.app.AlertDialog.Builder(MainActivity.this);
-
-                aDialog.setTitle("Login"); //타이틀바 제목
-                aDialog.setView(layout); //inti.xml 파일을 뷰로 셋팅
-                aDialog.setCancelable(true);
-                //그냥 닫기버튼을 위한 부분
-                aDialog.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                });
-                aDialog.setNeutralButton("회원가입", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-                aDialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-                //팝업창 생성
-                android.app.AlertDialog ad = aDialog.create();
-                ad.show();//보여줌!
+                showLoginPopup();
             }
         });
 
@@ -93,5 +60,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void showLoginPopup(){
+        Context mContext = getApplicationContext();
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        //R.layout.init는 xml 파일명이고  R.id.popup은 보여줄 레이아웃 아이디
+        View layout = inflater.inflate(R.layout.loginpopup,(ViewGroup) findViewById(R.id.login_popup));
+        final android.app.AlertDialog.Builder aDialog = new android.app.AlertDialog.Builder(MainActivity.this);
+
+        aDialog.setTitle("Login"); //타이틀바 제목
+        aDialog.setView(layout); //inti.xml 파일을 뷰로 셋팅
+        aDialog.setCancelable(true);
+        //그냥 닫기버튼을 위한 부분
+        aDialog.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        aDialog.setNeutralButton("회원가입", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                showJoinPopup();
+            }
+        });
+        aDialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        //팝업창 생성
+        android.app.AlertDialog ad = aDialog.create();
+        ad.show();//보여줌!
+    }
+    public void showJoinPopup(){
+        Context mContext = getApplicationContext();
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        //R.layout.init는 xml 파일명이고  R.id.popup은 보여줄 레이아웃 아이디
+        View layout = inflater.inflate(R.layout.joinpopup,(ViewGroup) findViewById(R.id.login_popup));
+        final android.app.AlertDialog.Builder aDialog = new android.app.AlertDialog.Builder(MainActivity.this);
+
+        aDialog.setTitle("Login"); //타이틀바 제목
+        aDialog.setView(layout); //inti.xml 파일을 뷰로 셋팅
+        aDialog.setCancelable(true);
+        //그냥 닫기버튼을 위한 부분
+        aDialog.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        aDialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        //팝업창 생성
+        android.app.AlertDialog ad = aDialog.create();
+        ad.show();//보여줌!
     }
 }
