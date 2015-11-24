1ccd5a6dc7db16af8f0415df851ab6d419f266fc
Kohyunmin <hyunmin4578@gmail.com>
2015-11-24 18:21:20 +0900

    Mysql connector 파일 추가 및 클래스 수정

--- app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/DAO.java
+++ app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/DAO.java
@@ -1,16 +1,19 @@
 package kr.hyunmin.shyboys.kr.hyunmin.object;
 
+import java.sql.*;
+
 /**
  * Created by Administrator on 2015-11-24.
  */
 public class DAO {
-    String _url;
-    String _conn;
-    String stmt;
-    String _rs;
-    String _user;
-    String _pass;
-    DTO _dto;
+    Connection _conn=null;
+    Statement stmt=null;
+    ResultSet _rs=null;
+    DTO DTO = null;
+
+    String _URL= "jdbc:mysql://localhost:3306/project"; // 사용하려는 데이터베이스명을 포함한
+    String _USER = "root"; // 사용자 계정
+    String _PASS = "root";
 
     public boolean addDB(){
         return false;
--- app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/Room.java
+++ app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/Room.java
@@ -4,4 +4,11 @@ package kr.hyunmin.shyboys.kr.hyunmin.object;
  * Created by Administrator on 2015-11-24.
  */
 public class Room {
+    public boolean create_room(){
+        return true;
+    }
+
+    public boolean exist_RC(){
+        return false;
+    }
 }
