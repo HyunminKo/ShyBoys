41c34d528a8982b5bae79103d2bff9c525d8d179
Seongjun2 <tjdwns4302@naver.com>
2015-11-24 18:09:20 +0900

    변수 underbar 추가

--- app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/Host.java
+++ app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/Host.java
@@ -4,9 +4,9 @@ package kr.hyunmin.shyboys.kr.hyunmin.object;
  * Created by Administrator on 2015-11-24.
  */
 public class Host {
-    public String room_code;
-    public String subject;
-    public String host_name;
-    public DAO dao;
-    public Room room;
+    public String _room_code;
+    public String _subject;
+    public String _host_name;
+    public DAO _dao;
+    public Room _room;
 }
