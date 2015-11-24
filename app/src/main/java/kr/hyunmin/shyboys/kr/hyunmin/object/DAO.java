d922cbaa72d6ad6ca060dc19f75eb4e9c7dffc1e
Kohyunmin <hyunmin4578@gmail.com>
2015-11-24 18:03:43 +0900

    DAO, DTO 빈 변수 메소드 추가

--- app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/DAO.java
+++ app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/DAO.java
@@ -4,4 +4,21 @@ package kr.hyunmin.shyboys.kr.hyunmin.object;
  * Created by Administrator on 2015-11-24.
  */
 public class DAO {
+    String _url;
+    String _conn;
+    String stmt;
+    String _rs;
+    String _user;
+    String _pass;
+    DTO _dto;
+
+    public boolean addDB(){
+        return false;
+    }
+    public boolean loadDB(){
+        return false;
+    }
+    public boolean deleteDB(){
+        return false;
+    }
 }
--- app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/DTO.java
+++ app/src/main/java/kr/hyunmin/shyboys/kr/hyunmin/object/DTO.java
@@ -4,4 +4,67 @@ package kr.hyunmin.shyboys.kr.hyunmin.object;
  * Created by Administrator on 2015-11-24.
  */
 public class DTO {
+    private String _ID;
+    private String _PW;
+    private String _room_code;
+    private int _QorA;
+    private int _QnA;
+    private String _content;
+    private String _date;
+
+    public String get_ID() {
+        return _ID;
+    }
+
+    public void set_ID(String _ID) {
+        this._ID = _ID;
+    }
+
+    public String get_PW() {
+        return _PW;
+    }
+
+    public void set_PW(String _PW) {
+        this._PW = _PW;
+    }
+
+    public String get_room_code() {
+        return _room_code;
+    }
+
+    public void set_room_code(String _room_code) {
+        this._room_code = _room_code;
+    }
+
+    public int get_QorA() {
+        return _QorA;
+    }
+
+    public void set_QorA(int _QorA) {
+        this._QorA = _QorA;
+    }
+
+    public int get_QnA() {
+        return _QnA;
+    }
+
+    public void set_QnA(int _QnA) {
+        this._QnA = _QnA;
+    }
+
+    public String get_content() {
+        return _content;
+    }
+
+    public void set_content(String _content) {
+        this._content = _content;
+    }
+
+    public String get_date() {
+        return _date;
+    }
+
+    public void set_date(String _date) {
+        this._date = _date;
+    }
 }
